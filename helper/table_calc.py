78cedfc9503b5a29cdeb7a2b1a275f61bb940490
trigork <trigork@gmail.com>
2019-06-13 10:07:38 +0200

    Removed some old helpers

--- helper/table_calc.py
+++
@@ -1,7 +0,0 @@
-letters = ['x', '0', '1', 'z', '2', 'i', 'o', '-']
-symbols = [1,2,3,4,6,7]
-
-print('A', 'B', '\t', '|', '&', '^',  '\t', 'a', 'b')
-for i in symbols:
-    for j in symbols:
-        print(letters[i], letters[j], '\t', (i | j), (i & j), (i ^ j), '\t', i, j)
